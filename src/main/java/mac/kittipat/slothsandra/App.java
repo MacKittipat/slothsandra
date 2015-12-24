7f4fd566f5adeea6674fc723685a102443deda60
Mac <mac@abctech-thailand.com>
2015-12-24 09:48:22 +0700

    Init project

---
+++ src/main/java/mac/kittipat/slothsandra/App.java
@@ -0,0 +1,13 @@
+package mac.kittipat.slothsandra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
