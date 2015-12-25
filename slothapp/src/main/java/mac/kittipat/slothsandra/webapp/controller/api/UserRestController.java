644e855ca61ef5f13e9ff99b5ec0f8e37789bfd1
Mac <mac@abctech-thailand.com>
2015-12-25 17:01:45 +0700

    Create REST endpoint for insert list of user

--- slothapp/src/main/java/mac/kittipat/slothsandra/webapp/controller/api/UserRestController.java
+++ slothapp/src/main/java/mac/kittipat/slothsandra/webapp/controller/api/UserRestController.java
@@ -1,11 +1,14 @@
 package mac.kittipat.slothsandra.webapp.controller.api;
 
 import mac.kittipat.slothsandra.core.dao.UserByChannelDao;
+import mac.kittipat.slothsandra.core.dao.UserDao;
+import mac.kittipat.slothsandra.core.model.User;
 import mac.kittipat.slothsandra.core.model.UserByChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,16 @@ import java.util.List;
 public class UserRestController {
 
     @Autowired
+    private UserDao userDao;
+
+    @Autowired
     private UserByChannelDao userByChannelDao;
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ResponseEntity create(@RequestBody List<User> userList) {
+        userList.forEach(user -> userDao.insert(user));
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 
     @RequestMapping(value = "/channels/{channelName}/users", method = RequestMethod.GET)
     public ResponseEntity<List<UserByChannel>> findByChannel(@PathVariable String channelName) {
