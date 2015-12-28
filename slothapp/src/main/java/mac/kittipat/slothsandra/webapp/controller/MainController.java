bd8b27e21b9e8876b0c3695d5d327ecf064d81f6
Mac <mac@abctech-thailand.com>
2015-12-28 17:55:32 +0700

    Using JS to display message

--- slothapp/src/main/java/mac/kittipat/slothsandra/webapp/controller/MainController.java
+++ slothapp/src/main/java/mac/kittipat/slothsandra/webapp/controller/MainController.java
@@ -5,9 +5,7 @@ import mac.kittipat.slothsandra.core.dao.MessageByChannelDao;
 import mac.kittipat.slothsandra.core.dao.UserByChannelDao;
 import mac.kittipat.slothsandra.core.dao.YearByChannelDao;
 import mac.kittipat.slothsandra.core.model.Channel;
-import mac.kittipat.slothsandra.core.model.MessageByChannel;
 import mac.kittipat.slothsandra.core.model.UserByChannel;
-import mac.kittipat.slothsandra.core.model.YearByChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -49,35 +44,9 @@ public class MainController {
 
     @RequestMapping(value = "/channel/{channelName}")
     public String channel(Model model,
-                          @PathVariable String channelName,
-                          @RequestParam(required = false) Long createdTime,
-                          @RequestParam(required = false) Integer limit) {
+                          @PathVariable String channelName) {
         List<UserByChannel> userByChannelList = userByChannelDao.findUserByChannel(channelName);
         model.addAttribute("userByChannelList", userByChannelList);
-
-        if(createdTime == null) {
-            createdTime = Instant.now().toEpochMilli();
-        }
-        if(limit == null) {
-            limit = DEFAULT_LIMIT;
-        }
-
-        int yearIndex = 0;
-        List<YearByChannel> yearByChannelList = yearByChannelDao.findByChannel(channelName);
-        List<MessageByChannel> messageByChannelList = new ArrayList<>();
-        while (messageByChannelList.size() < limit && yearIndex < yearByChannelList.size()) {
-            List<MessageByChannel> messageByChannelListTemp =
-                    messageByChannelDao.findMessageByChannel(
-                            channelName,
-                            yearByChannelList.get(yearIndex).getYearByChannelKey().getYear(),
-                            createdTime,
-                            limit - messageByChannelList.size());
-            messageByChannelList.addAll(messageByChannelListTemp);
-            yearIndex++;
-        }
-        model.addAttribute("messageByChannelList", messageByChannelList);
-        model.addAttribute("latestCreatedTime", messageByChannelList.get(messageByChannelList.size() - 1)
-                .getMessageByChannelKey().getCreatedTime().getTime());
         return "channel";
     }
 
--- slothapp/src/main/resources/templates/channel.ftl
+++ slothapp/src/main/resources/templates/channel.ftl
@@ -19,28 +19,42 @@
     </div>
     <div style="float: left; width: 70%;">
         <ul id="messages-list">
-        <#list messageByChannelList as messageByChannel>
-            <li>${messageByChannel.message}</li>
-        </#list>
         </ul>
     </div>
     <script type="text/javascript" src="/slothsandra/assets/jquery/jquery-2.1.4.min.js"></script>
     <script type="text/javascript">
-        var latestCreatedTime = ${latestCreatedTime?long?c};
+
+        var latestCreatedTime = 0;
+
+        var cb = function(data) {
+            for(var i=0; i<data.length; i++) {
+                $('#messages-list').append('<li>(' + data[i].messageByChannelKey.createdTime + ') ' + data[i].username + ' : ' + escapeHtml(data[i].message) + '</li>')
+            }
+            if(data.length > 0) {
+                latestCreatedTime =  data[data.length - 1].messageByChannelKey.createdTime;
+            } else {
+                latestCreatedTime = 0;
+            }
+        }
+
+        $(document).ready(function() {
+            $.get('http://localhost:9000/slothsandra/api/channels/${channelName}/messages', cb);
+        });
+
         $(window).scroll(function() {
             if($(window).scrollTop() + $(window).height() == $(document).height() && latestCreatedTime != 0) {
-                $.get('http://localhost:9000/slothsandra/api/channels/general/messages?createdTime=' + latestCreatedTime, function(data) {
-                    for(var i=0; i<data.length; i++) {
-                        $('#messages-list').append('<li>' + data[i].message + '</li>')
-                    }
-                    if(data.length > 0) {
-                        latestCreatedTime =  data[data.length - 1].messageByChannelKey.createdTime;
-                    } else {
-                        latestCreatedTime = 0;
-                    }
-                });
+                $.get('http://localhost:9000/slothsandra/api/channels/${channelName}/messages?createdTime=' + latestCreatedTime, cb);
             }
         });
+
+        function escapeHtml(unsafe) {
+            return unsafe
+                    .replace(/&/g, "&amp;")
+                    .replace(/</g, "&lt;")
+                    .replace(/>/g, "&gt;")
+                    .replace(/"/g, "&quot;")
+                    .replace(/'/g, "&#039;");
+        }
     </script>
 </body>
 </html>
