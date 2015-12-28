028bafe941612199f3698ecfd0e9896daf68c0ea
Mac <mac@abctech-thailand.com>
2015-12-28 16:25:14 +0700

    Move materialize to assets folder

--- slothapp/src/main/java/mac/kittipat/slothsandra/webapp/controller/MainController.java
+++ slothapp/src/main/java/mac/kittipat/slothsandra/webapp/controller/MainController.java
@@ -3,8 +3,11 @@ package mac.kittipat.slothsandra.webapp.controller;
 import mac.kittipat.slothsandra.core.dao.ChannelDao;
 import mac.kittipat.slothsandra.core.dao.MessageByChannelDao;
 import mac.kittipat.slothsandra.core.dao.UserByChannelDao;
+import mac.kittipat.slothsandra.core.dao.YearByChannelDao;
 import mac.kittipat.slothsandra.core.model.Channel;
+import mac.kittipat.slothsandra.core.model.MessageByChannel;
 import mac.kittipat.slothsandra.core.model.UserByChannel;
+import mac.kittipat.slothsandra.core.model.YearByChannel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +15,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class MainController {
 
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    private static final int DEFAULT_LIMIT = 50;
 
     @Autowired
     private ChannelDao channelDao;
@@ -29,6 +37,8 @@ public class MainController {
     @Autowired
     private MessageByChannelDao messageByChannelDao;
 
+    @Autowired
+    private YearByChannelDao yearByChannelDao;
 
     @RequestMapping(value = "/")
     public String index(Model model) {
@@ -38,11 +48,34 @@ public class MainController {
     }
 
     @RequestMapping(value = "/channel/{channelName}")
-    public String channel(Model model, @PathVariable String channelName) {
+    public String channel(Model model,
+                          @PathVariable String channelName,
+                          @RequestParam(required = false) Long createdTime,
+                          @RequestParam(required = false) Integer limit) {
         List<UserByChannel> userByChannelList = userByChannelDao.findUserByChannel(channelName);
-//        List<MessageByChannel> messageByChannelList = messageByChannelDao.findMessageByChannel(channelName);
         model.addAttribute("userByChannelList", userByChannelList);
-//        model.addAttribute("messageByChannelList", messageByChannelList);
+
+        if(createdTime == null) {
+            createdTime = Instant.now().toEpochMilli();
+        }
+        if(limit == null) {
+            limit = DEFAULT_LIMIT;
+        }
+
+        int yearIndex = 0;
+        List<YearByChannel> yearByChannelList = yearByChannelDao.findByChannel(channelName);
+        List<MessageByChannel> messageByChannelList = new ArrayList<>();
+        while (messageByChannelList.size() < limit && yearIndex < yearByChannelList.size()) {
+            List<MessageByChannel> messageByChannelListTemp =
+                    messageByChannelDao.findMessageByChannel(
+                            channelName,
+                            yearByChannelList.get(yearIndex).getYearByChannelKey().getYear(),
+                            createdTime,
+                            limit - messageByChannelList.size());
+            messageByChannelList.addAll(messageByChannelListTemp);
+            yearIndex++;
+        }
+        model.addAttribute("messageByChannelList", messageByChannelList);
         return "channel";
     }
 
