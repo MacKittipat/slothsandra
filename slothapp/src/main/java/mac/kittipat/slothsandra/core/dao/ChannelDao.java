33c33770a96b47eee69477e4361795fec6e4cb7e
Mac <mac@abctech-thailand.com>
2015-12-29 16:28:05 +0700

    Add column message_counter

--- slothapp/src/main/java/mac/kittipat/slothsandra/core/dao/ChannelDao.java
+++ slothapp/src/main/java/mac/kittipat/slothsandra/core/dao/ChannelDao.java
@@ -1,5 +1,7 @@
 package mac.kittipat.slothsandra.core.dao;
 
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Update;
 import mac.kittipat.slothsandra.core.model.Channel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraTemplate;
@@ -14,7 +16,14 @@ public class ChannelDao {
     private CassandraTemplate cassandraTemplate;
 
     public Channel insert(Channel channel) {
-        return cassandraTemplate.insert(channel);
+        // Use update because this table have counter column. insert does not work if table have counter column.
+        Update update = QueryBuilder.update("channel");
+        update.with(QueryBuilder.incr("count_message", 0)); // Count start with zero.
+        update.where(QueryBuilder.eq("slack_name", "abctech"));
+        update.where(QueryBuilder.eq("channel_name", channel.getChannelKey().getChannelName()));
+        update.where(QueryBuilder.eq("channel_id", channel.getChannelKey().getChannelId()));
+        cassandraTemplate.execute(update);
+        return channel;
     }
 
     public List<Channel> findAll() {
--- slothapp/src/main/java/mac/kittipat/slothsandra/core/dao/MessageDao.java
+++ slothapp/src/main/java/mac/kittipat/slothsandra/core/dao/MessageDao.java
@@ -3,12 +3,15 @@ package mac.kittipat.slothsandra.core.dao;
 import com.datastax.driver.core.BatchStatement;
 import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Update;
 import com.datastax.driver.core.utils.UUIDs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.Resource;
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.UUID;
 
 @Repository
@@ -17,15 +20,34 @@ public class MessageDao {
     @Autowired
     private CassandraTemplate cassandraTemplate;
 
+    @Resource
+    private Map<String, String> channelIdMap;
+
     public void insert(String channelName, String username, String message, Long createdTime) {
 
         int year = LocalDate.now().getYear();
         // Use startOf because compare uuid_time<minTimeuuid()
         UUID uuidTime = UUIDs.startOf(createdTime);
 
-        Insert insertUserByChannel = QueryBuilder.insertInto("user_by_channel");
-        insertUserByChannel.value("channel_name", channelName);
-        insertUserByChannel.value("username", username);
+        // Use 2 batch because counter operation must be UNLOGGED but other operation are LOGGED by default.
+
+        // Cannot include counter in batch because other operation are not update counter
+        // and counter does not work with LOGGED batch.
+        Update updateCounterChannel = QueryBuilder.update("channel");
+        updateCounterChannel.with(QueryBuilder.incr("count_message", 1));
+        updateCounterChannel.where(QueryBuilder.eq("slack_name", "abctech"));
+        updateCounterChannel.where(QueryBuilder.eq("channel_name", channelName));
+        updateCounterChannel.where(QueryBuilder.eq("channel_id", findChannelIdByChannelName(channelName)));
+
+        Update updateCounterUserByChannel = QueryBuilder.update("user_by_channel");
+        updateCounterUserByChannel.with(QueryBuilder.incr("count_message", 1));
+        updateCounterUserByChannel.where(QueryBuilder.eq("channel_name", channelName));
+        updateCounterUserByChannel.where(QueryBuilder.eq("username", username));
+
+        BatchStatement batchCounter = new BatchStatement(BatchStatement.Type.UNLOGGED);
+        batchCounter.add(updateCounterChannel);
+        batchCounter.add(updateCounterUserByChannel);
+        cassandraTemplate.execute(batchCounter);
 
         Insert insertMessageByChannel = QueryBuilder.insertInto("message_by_channel");
         insertMessageByChannel.value("channel_name", channelName);
@@ -49,11 +71,17 @@ public class MessageDao {
 
         // Use batch for atomicity. Keep data in all table consistency.
         BatchStatement batch = new BatchStatement();
-        batch.add(insertUserByChannel);
         batch.add(insertMessageByChannel);
         batch.add(insertMessageByUserChannel);
         batch.add(insertYearByChannel);
-
         cassandraTemplate.execute(batch);
+    }
+
+    private String findChannelIdByChannelName(String channelName) {
+        return channelIdMap.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(channelName))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
     }
 }
